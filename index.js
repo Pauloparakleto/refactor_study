1aaa0d4e152ff9ffe814206e4021bfe850a27902
paulo-felipe <alparakleto@gmail.com>
2023-04-21 20:14:15 -0300

    fix(index.js): remove the thisAmount argument since it is declared inside amountFor.
    This is the first moment when the test case failed. Good Job jest!

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function statement(invoice, plays) {
     for(let perf of invoice.performances){
         const play = plays[perf.playID];
         let thisAmount = 0;
-        thisAmount = amountFor(play, thisAmount, perf);
+        thisAmount = amountFor(play, perf);
 
         // Add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
