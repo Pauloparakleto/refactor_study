c671cc411ba8e8bf49d3f8ddec0e2f3fc1cf70c4
paulo-felipe <alparakleto@gmail.com>
2023-04-21 19:59:59 -0300

    refactor(index.js): Extract the nested switch inside the for loop. This is an example of nested dependency first step to solve. The way it was before,any change in the for loop outside or even in the invoice play variables above could break the switch code fragment.
    According to the book, to extract it we need to check if the variables present in the code fragment will lose its reference declaration.
    
    I opted, initially to declare "thisAmount" out of amountFor method and just pass it down to this extracted method. This is a little divergence from the book. However, it can be declared inside amountFor method according to your preference.
    
    My first conclusion here is: We must start from the most nested code fragment ever. I need to confirm it later.

--- index.js
+++ index.js
@@ -7,24 +7,7 @@ function statement(invoice, plays) {
     for(let perf of invoice.performances){
         const play = plays[perf.playID];
         let thisAmount = 0;
-
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 *(perf.audience - 20);
-                }
-                thisAmount += 300 * (perf.audience);
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
+        thisAmount = amountFor(play, thisAmount, perf);
 
         // Add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -43,5 +26,27 @@ function statement(invoice, plays) {
 
     return result;
 }
+
+function amountFor(play, thisAmount, perf) {
+    switch (play.type) {
+        case "tragedy":
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+        case "comedy":
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * (perf.audience);
+            break;
+        default:
+            throw new Error(`unknown type: ${play.type}`);
+    }
+    return thisAmount;
+}
+
 
 module.exports = statement;
