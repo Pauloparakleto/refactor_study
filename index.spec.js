9606eed02379c9f8710f7ef72407959f80522236
paulo-felipe <alparakleto@gmail.com>
2023-04-21 19:51:15 -0300

    refactor(index.spec.js): describe better the expectation

--- index.spec.js
+++ index.spec.js
@@ -2,7 +2,7 @@ const statement = require('./index');
 const invoices = require('./invoices.json');
 const plays = require('./plays.json');
 
-test('adds 1 + 2 to equal 3', () => {
+test('It returns correct message', () => {
   let message = "Statement for BigCo" +
       "\n Hamlet: $650.00 (55 seats)" +
       "\n As You like it: $580.00 (35 seats)" +
